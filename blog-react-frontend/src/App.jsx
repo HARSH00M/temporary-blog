62ba6915737caf374f68992be416b257e6720d2c
Harsh00M <harsh10mahobia@gmail.com>
2024-07-17 13:36:03 +0530

    changed routing

--- blog-react-frontend/src/App.jsx
+++ blog-react-frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
 
     <Routes>
-      <Route path="/" element={<Home/>}>
+      <Route element={<Home/>}>
         
       <Route path="/" element={<Banner/>} />
       <Route path="/contact" element={<Contact/>} />
